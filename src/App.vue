7acf3ce9082ab039e99fbff8b3ceef51747dc6ac
juni <woajun@naver.com>
2022-08-30 09:23:51 +0900

    set interface

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
-<script setup lang="ts">
+<script setup lang="ts">import { ref } from 'vue';
 
-  
+interface Person {
+  name: string
+  schedule: Schedule[]
+}
+
+interface Schedule {
+  type: 'day' | 'night'
+  date: Date
+  asign: Person
+}
+
+interface Day {
+  day: Schedule
+  night: Schedule
+  holiday: boolean
+}
+
+type Calculator = (people: Person[], length: number, holiday: number[]) => Day[]
+    
 </script>
 
 <template>
-  <main>
-    sss
-  </main>
+    <div>
+      <select>
+        <option value="2022">2022</option>
+      </select>
+      <select>
+        <option value="9">9</option>
+      </select>
+      월
+    </div>
+    <div>
+      근무자 수 : 
+      <select>
+        <option value="6">6</option>
+      </select>
+    </div>
+    <div><button @click="calc">계산</button></div>
+    <div>{{result}}</div>
 </template>
