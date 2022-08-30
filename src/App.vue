da3eb109163901f6995445cb5ec4ec9f34fca16b
juni <woajun@naver.com>
2022-08-30 15:07:26 +0900

    calc rest workday

--- src/App.vue
+++ src/App.vue
@@ -46,8 +46,20 @@ const makePeople = (names: string[], wl: number, hl: number, shifts: string[], p
   /** 주말과 평일을 비율로 나눈다 */
   const weekdayHaveTo = oneHaveTo * (wl / (wl + hl));
   const holidayHaveTo = oneHaveTo * (hl / (wl + hl));
-  console.log(weekdayHaveTo);
-  console.log(holidayHaveTo);
+  console.log('weekdayHaveTo', weekdayHaveTo);
+  console.log('holidayHaveTo', holidayHaveTo);
+
+  /** 내림한다. */
+  const weekdayNum = Math.floor(weekdayHaveTo);
+  const holidayNum = Math.floor(holidayHaveTo);
+  console.log('weekdayNum', weekdayNum);
+  console.log('holidayNum', holidayNum);
+
+  /** 잔업량을 구한다 */
+  const restWeekdayNum = (weekdayHaveTo * names.length) - (weekdayNum * names.length);
+  const restholidayNum = (holidayHaveTo * names.length) - (holidayNum * names.length);
+  console.log('restWeekdayNum', restWeekdayNum);
+  console.log('restholidayNum', restholidayNum);
 };
 
 const onClick = () => {
@@ -59,6 +71,15 @@ const onClick = () => {
   const perShift = 2;
   const people = makePeople(names, workdays.length, holidays.length, shifts, perShift);
   const assigned = assignDate(people);
+
+  // const inputNames = ['홍길동', '유재석', '박명수', '정준하', '노홍철', '정형돈'];
+  // const names = shuffle(inputNames);
+  // const workdays = [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31];
+  // const holidays = [6, 7, 13, 14, 20, 21, 27, 28];
+  // const shifts = ['낮', '저녁', '초저녁'];
+  // const perShift = 1;
+  // const people = makePeople(names, workdays.length, holidays.length, shifts, perShift);
+  // const assigned = assignDate(people);
 };
 
 </script>
