13b20644151cd1bfc3c9fc6d1e90b047b431ae2e
juni <woajun@naver.com>
2022-08-31 12:24:20 +0900

    mergeDayHoli

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable @typescript-eslint/no-array-constructor -->
 <!-- eslint-disable arrow-body-style -->
 <!-- eslint-disable vue/max-len -->
 <!-- eslint-disable no-multiple-empty-lines -->
@@ -119,87 +120,37 @@ const distribute = (date:number, shifts: number, people: number) => {
   return iterate(people).map((i) => arrs.map((e) => e[i]));
 };
 
-const makePeople = (names: string[], workdays: number, partTimeTypes: PartTimeType[], perShift:number) => {
-  const numOfPerson = names.length;
-  const numOfShift = partTimeTypes.length;
+const mergeDayHoli = (arrA:number[][], arrB:number[][]) => {
+  arrB.reverse();
+  return iterate(arrA.length).map((i) => [arrA[i], arrB[i]]);
+};
 
-  /** 사람 수대로 나눈다. */
-  const oneHaveTo = (workdays * numOfShift * perShift) / numOfPerson;
-  console.log('oneHaveTo', oneHaveTo);
-
-  /** 내림하여 한 사람 당 실제 공수를 구한다. */
-  const realHaveTo = Math.floor(oneHaveTo);
-  console.log('realHaveTo', realHaveTo);
-
-  /** 사람 수대로 나눈 것을 배열로 만든다. */
-  const people = names.map((name) => ({
-    id: newID(),
-    name,
-    realHaveTo,
-  }));
-  console.log(people);
-
-  /** 잔업량을 구한다 */
-  const rest = (oneHaveTo * names.length) - (realHaveTo * names.length);
-  console.log('rest', rest);
-
-  /** 잔업을 분배한다. */
-  Array(rest).fill(1).forEach((e, i) => {
-    people[i].realHaveTo = people[i].realHaveTo + e;
-  });
-
-  console.log('people', people);
-
-  console.log('distribute', distribute(30, 2, 4));
-
-  // /** 교대근무로 나눈다 */
-  // const applePeople = people.map((e, i) => {
-  //   const flag = i % 2 === 0;
-  //   const apple = e.realHaveTo / numOfShift;
-  //   let weekdayHaveTo;
-  //   if (apple % 1 === 0) {
-  //     weekdayHaveTo = partTimeTypes.map((type) => ({
-  //       type,
-  //       number: apple,
-  //     }));
-  //     // TODO
-  //     // 7.5 이면 (8, 7) 나누는 로직
-  //     // 7.6666 이면 (7, 8, 8)
-  //     // 7.3333 이면 (7, 7, 8)
-  //   } else {
-  //     console.log(apple);
-  //   }
-  //   return {
-  //     id: e.id,
-  //     name: e.name,
-  //     weekdayHaveTo,
-  //   };
-  // });
-  // console.log('applePeople', applePeople);
-
-
-
-
-  return applePeople;
+const makePeople = (names: string[], dayWork: number, holiWrok: number, partTimeTypes: PartTimeType[], perShift:number) => {
+  const dayWorkArr = distribute(dayWork * perShift, partTimeTypes.length, names.length);
+  console.log('dayWorkArr', dayWorkArr);
+  const holiWorkArr = distribute(holiWrok * perShift, partTimeTypes.length, names.length);
+  console.log('holiWorkArr', holiWorkArr);
+  console.log('merge day+holi', mergeDayHoli(dayWorkArr, holiWorkArr));
 };
 
 const onClick = () => {
-  console.log('1번', distribute(15, 2, 4));
-  console.log('2번', distribute(20, 3, 3));
-  console.log('3번', distribute(19, 3, 3));
-  console.log('4번', distribute(19, 3, 4));
-  console.log('5번', distribute(30, 2, 4));
-  // const workdays = [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31];
-  // const holidays = [6, 7, 13, 14, 20, 21, 27, 28];
-  // const shifts = ['낮', '저녁'];
-  // const perShift = 2;
+  // console.log('1번', distribute(15, 2, 4));
+  // console.log('2번', distribute(20, 3, 3));
+  // console.log('3번', distribute(19, 3, 3));
+  // console.log('4번', distribute(19, 3, 4));
+  // console.log('5번', distribute(30, 2, 4));
 
-  // const partTimeType = makePartTimeTypes(shifts);
-  // const calendar = makeCalendar(workdays, holidays, partTimeType, perShift);
+  const workdays = [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31];
+  const holidays = [6, 7, 13, 14, 20, 21, 27, 28];
+  const shifts = ['낮', '저녁'];
+  const perShift = 2;
 
-  // const inputNames = ['홍길동', '유재석', '박명수', '정준하', '노홍철', '임꺽정'];
-  // const names = shuffle(inputNames);
-  // const people = makePeople(names, workdays.length, partTimeType, perShift);
+  const partTimeType = makePartTimeTypes(shifts);
+  const calendar = makeCalendar(workdays, holidays, partTimeType, perShift);
+
+  const inputNames = ['홍길동', '유재석', '박명수', '정준하', '노홍철', '임꺽정'];
+  const names = shuffle(inputNames);
+  const people = makePeople(names, workdays.length, holidays.length, partTimeType, perShift);
 
   // console.log(calendar);
 };
