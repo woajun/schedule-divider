6150d548387098ba358e897d4d32d818838686cf
juni <woajun@naver.com>
2022-08-31 16:37:03 +0900

    add start end index

--- src/components/SpecifyDate/DateButtonTable.vue
+++ src/components/SpecifyDate/DateButtonTable.vue
@@ -13,17 +13,35 @@ interface Day {
 const props = defineProps<{ array: Day[] }>();
 
 // 모든 일요일 인덱스를 찾는다.
-const everySundayIndexes = computed(() => props.array.reduce((arr, e, i) => {
+const findSunIndexes = (array: Day[]) => array.reduce((arr, e, i) => {
   if (e.weekday === 0) arr.push(i);
   return arr;
-}, [] as number[]));
+}, [] as number[]);
 
-console.log('everySundayIndexes', everySundayIndexes.value);
+const addStartEndIndex = (array:Day[]) => {
+  const sundays = array.reduce((arr, e, i) => {
+    if (e.weekday === 0) arr.push(i);
+    return arr;
+  }, [] as number[]);
+  const orange = sundays.includes(0) ? sundays : [0, ...sundays];
+  const lastIndex = array.length - 1;
+  const banana = orange.includes(lastIndex) ? orange : [...orange, lastIndex];
+  return banana;
+};
+
+// 일요일 인덱스로 props.array를 쪼갠다.
+const splited = computed(() => {
+  const arr = props.array;
+  const sunIndexes = findSunIndexes(arr);
+  // 시작, 마지막, 일요일 인덱스를 찾는다.
+  const apple = addStartEndIndex(arr);
+  console.log('apple', apple);
+
+  return [];
+});
 
 // 인덱스를 기준으로 배열을 만든다.
-// 배열을 템플릿에서 돌린다.
-
-const month = computed(() => {
+const computedMonth = computed(() => {
   const months = iterate(6).map((i) => ({
     0: undefined,
     1: undefined,
@@ -38,10 +56,12 @@ const month = computed(() => {
   return months;
 });
 
+// 배열을 템플릿에서 돌린다.
+
 </script>
 <template>
   <div>
-    {{ everySundayIndexes }}
+    {{ splited }}
     <table>
       <thead>
         <tr>
@@ -56,7 +76,7 @@ const month = computed(() => {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(week, i) in month" :key="i">
+        <tr v-for="(week, i) in computedMonth" :key="i">
           <th>{{ i + 1 }}</th>
           <td v-if="week[0]">
             <button>aaa</button>
